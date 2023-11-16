411ade9acb24b3bf66c1ad95dd49f30b89c9f5aa
Ritwik Sood <43468629+ritwiksood20@users.noreply.github.com>
2023-11-16 15:37:31 -0500

    Update JobPosting.js

--- src/components/JobPosting.js
+++ src/components/JobPosting.js
@@ -1,116 +1,119 @@
 import React, { useState } from 'react';
-import '../styles/JobPosting.css';
+import './JobPosting.css';
 
 function App() {
-  const [jobs, setJobs] = useState([]);
-  const [newJob, setNewJob] = useState({
-    postId: '',
-    company: '',
-    position: '',
-    location: '',
-    seniority: '',
-    description: '',
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setNewJob({ ...newJob, [name]: value });
-  };
-
-  const handleAddJob = () => {
-    setJobs([...jobs, newJob]);
-    setNewJob({
-      postId: '',
-      company: '',
-      position: '',
-      location: '',
-      seniority: '',
-      description: '',
+    const [newJob, setNewJob] = useState({
+        postId: '',
+        company: '',
+        position: '',
+        location: '',
+        seniority: '',
+        description: '',
     });
-  };
 
-  return (
-      <div className="App">
-        <h1><center>Job Posting Page</center></h1>
-        <div>
-            <label htmlFor="postId"><b>Post ID: </b></label>
-          <input
-              type="text"
-              id="postId"
-              name="postId"
-              value={newJob.postId}
-              onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="company"><b>Company: </b></label>
-          <input
-              type="text"
-              id="company"
-              name="company"
-              value={newJob.company}
-              onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="position"><b>Position: </b></label>
-          <input
-              type="text"
-              id="position"
-              name="position"
-              value={newJob.position}
-              onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="location"><b>Location: </b></label>
-          <input
-              type="text"
-              id="location"
-              name="location"
-              value={newJob.location}
-              onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="seniority"><b>Seniority: </b></label>
-          <input
-              type="text"
-              id="seniority"
-              name="seniority"
-              value={newJob.seniority}
-              onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="description"><b>Description: </b></label>
-          <textarea
-              id="description"
-              name="description"
-              value={newJob.description}
-              onChange={handleInputChange}
-          />
-        </div>
-        <button onClick={handleAddJob}>Add Job</button>
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setNewJob({ ...newJob, [name]: value });
+    };
 
-          <div className="job-list-container">
-              <h2>Job Listings:</h2>
-              <ol className="job-list">
-                  {jobs.map((job, index) => (
-                      <li key={index} className="job-item">
-                          <strong>Post ID:</strong> {job.postId},{' '}
-                          <strong>Company:</strong> {job.company},{' '}
-                          <strong>Position:</strong> {job.position},{' '}
-                          <strong>Location:</strong> {job.location},{' '}
-                          <strong>Seniority:</strong> {job.seniority},{' '}
-                          <strong>Description:</strong> {job.description}
-                      </li>
-                  ))}
-              </ol>
-          </div>
+    const handleAddJob = () => {
 
-      </div>
-  );
+        sendJSONData(newJob);
+        setNewJob({
+            postId: '',
+            company: '',
+            position: '',
+            location: '',
+            seniority: '',
+            description: '',
+        });
+    };
+
+    const sendJSONData = (data) => {
+        const apiUrl = 'http://localhost:3000/addJobPosting'; // Update with your server's URL
+
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((responseData) => {
+                // Handle the response as needed
+                console.log('Response from server:', responseData);
+            })
+            .catch((error) => {
+                console.error('Error sending data:', error);
+            });
+    };
+
+    return (
+        <div className="app-container">
+            <h1><center>Job Posting Page</center></h1>
+            <div className="form-container">
+                <label htmlFor="postId">Post ID:</label>
+                <input
+                    type="text"
+                    id="postId"
+                    name="postId"
+                    value={newJob.postId}
+                    onChange={handleInputChange}
+                />
+                <div>
+                    <label htmlFor="company"><b>Company: </b></label>
+                    <input
+                        type="text"
+                        id="company"
+                        name="company"
+                        value={newJob.company}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="position"><b>Position: </b></label>
+                    <input
+                        type="text"
+                        id="position"
+                        name="position"
+                        value={newJob.position}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="location"><b>Location: </b></label>
+                    <input
+                        type="text"
+                        id="location"
+                        name="location"
+                        value={newJob.location}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="seniority"><b>Seniority: </b></label>
+                    <input
+                        type="text"
+                        id="seniority"
+                        name="seniority"
+                        value={newJob.seniority}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="description"><b>Description: </b></label>
+                    <textarea
+                        id="description"
+                        name="description"
+                        value={newJob.description}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <button onClick={handleAddJob}>Add Job</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
